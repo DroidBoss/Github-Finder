b715eae063569f95d31b84c06dc7548660871151
Anshu Verma <verma.anshu96@gmail.com>
2021-08-21 12:39:29 +0530

    Added Home and Page not found component. App finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { Fragment } from "react";
+import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
-import Users from "./components/Users";
-import Search from "./components/Search";
+import Home from "./components/Home";
 import Alert from "./components/Alert";
 import About from "./components/About";
 import User from "./components/User";
+import NotFound from "./components/NotFound";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import GithubState from "./context/github/GithubState";
 import AlertState from "./context/alert/AlertState";
@@ -20,18 +20,10 @@ const App = () => {
             <div className="container">
               <Alert />
               <Switch>
-                <Route
-                  exact
-                  path="/"
-                  render={(props) => (
-                    <Fragment>
-                      <Search />
-                      <Users />
-                    </Fragment>
-                  )}
-                />
+                <Route exact path="/" component={Home} />
                 <Route exact path="/about" component={About} />
                 <Route exact path="/user/:login" component={User} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
